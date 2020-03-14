67bf8e93eba99a88e1bd0d2267680ac9dc597d32
Maximilian Siling <mouse-art@ya.ru>
2020-03-14 15:20:54 +0300

    cargo fmt

--- src/dispatching/dialogue/storage/redis.rs
+++ src/dispatching/dialogue/storage/redis.rs
@@ -3,12 +3,11 @@
 
 use super::Storage;
 use futures::future::BoxFuture;
-use std::sync::Arc;
 use redis::{AsyncCommands, IntoConnectionInfo};
+use serde::{de::DeserializeOwned, Serialize};
+use std::sync::Arc;
 use thiserror::Error;
-use serde::{Serialize, de::DeserializeOwned};
 pub use Serializer::*;
-
 
 #[derive(Debug, Error)]
 pub enum Error {
@@ -26,76 +25,73 @@ pub enum Serializer {
 
 impl Serializer {
     pub fn serialize<D>(&self, val: &D) -> Result<Vec<u8>>
-    where D: Serialize {
+    where
+        D: Serialize,
+    {
         Ok(match self {
-            JSON => serde_json::to_vec(val)?
+            JSON => serde_json::to_vec(val)?,
         })
     }
 
     pub fn deserialize<'de, D>(&self, data: &'de [u8]) -> Result<D>
-    where D: DeserializeOwned {
+    where
+        D: DeserializeOwned,
+    {
         Ok(match self {
-            JSON => serde_json::from_slice(data)?
+            JSON => serde_json::from_slice(data)?,
         })
     }
 }
 
 pub struct RedisStorage {
     client: redis::Client,
-    serializer: Serializer
+    serializer: Serializer,
 }
 
 impl RedisStorage {
-    pub fn open(url: impl IntoConnectionInfo, serializer: Serializer) -> Result<Self> {
-        Ok(Self {
-            client: redis::Client::open(url)?,
-            serializer,
-        })
+    pub fn open(
+        url: impl IntoConnectionInfo,
+        serializer: Serializer,
+    ) -> Result<Self> {
+        Ok(Self { client: redis::Client::open(url)?, serializer })
     }
 }
 
-impl<D> Storage<D> for RedisStorage 
-where D: Send + Serialize + DeserializeOwned + 'static {
+impl<D> Storage<D> for RedisStorage
+where
+    D: Send + Serialize + DeserializeOwned + 'static,
+{
     type Error = Error;
 
     fn remove_dialogue(
         self: Arc<Self>,
-        chat_id: i64
+        chat_id: i64,
     ) -> BoxFuture<'static, Result<Option<D>>> {
-        Box::pin(
-            async move {
-                let mut conn = self.client.get_async_connection().await?;
-                let old_val: Option<D> = conn
-                    .get::<_, Option<Vec<u8>>>(chat_id)
-                    .await?
-                    .map(|d| self.serializer.deserialize(&d))
-                    .transpose()?;
-                conn.del(chat_id).await?;
-                Ok(old_val)
-            }
-        )
+        Box::pin(async move {
+            let mut conn = self.client.get_async_connection().await?;
+            let old_val: Option<D> = conn
+                .get::<_, Option<Vec<u8>>>(chat_id)
+                .await?
+                .map(|d| self.serializer.deserialize(&d))
+                .transpose()?;
+            conn.del(chat_id).await?;
+            Ok(old_val)
+        })
     }
 
     fn update_dialogue(
         self: Arc<Self>,
         chat_id: i64,
-        dialogue: D
+        dialogue: D,
     ) -> BoxFuture<'static, Result<Option<D>>> {
-        Box::pin(
-            async move {
-                let mut conn = self.client.get_async_connection().await?;
-                let dialogue = self.serializer.serialize(&dialogue)?;
-                Ok(
-                    conn
-                        .getset::<_, Vec<u8>, Option<Vec<u8>>>(
-                            chat_id,
-                            dialogue
-                        )
-                        .await?
-                        .map(|d| self.serializer.deserialize(&d))
-                        .transpose()?
-                )
-            }
-        )
+        Box::pin(async move {
+            let mut conn = self.client.get_async_connection().await?;
+            let dialogue = self.serializer.serialize(&dialogue)?;
+            Ok(conn
+                .getset::<_, Vec<u8>, Option<Vec<u8>>>(chat_id, dialogue)
+                .await?
+                .map(|d| self.serializer.deserialize(&d))
+                .transpose()?)
+        })
     }
 }
